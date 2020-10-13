e04e9865833a81fd56cddf013698218d385275cb
jose00796 <josebalboa11@hotmail.com>
2020-10-13 15:15:40 -0400

    Autocarga de PHP completa papu

--- index.php
+++ index.php
@@ -6,5 +6,11 @@ SE QUE LO LOGRARAS IGUAL QUE SIEMPRE.*/
 
 /* 13/10/2020 AHORA SI VAMOS A HACER ESTA MIERDA BIEN NOJODA QUE LADILLA... */
 
+spl_autoload_register(function($classname){
+        if (file_exists("src/$classname.php")) {
+            require "src/$classname.php";
+        }
+});
+
 
 
